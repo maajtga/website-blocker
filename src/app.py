9c43c4f6b4b7fdbf63866e191b106c116fca9de4
maajtga <maajtga@users.noreply.github.com>
2021-02-18 09:39:11 -0500

    Update app.py

--- src/app.py
+++ src/app.py
@@ -5,13 +5,21 @@ import sys
 #initialize the window
 window = tk.Tk()
 
-#create a label
-title = tk.Label(window, text = "Website Blocker", font=("Helvetica", 18))
-title.place(x=150,y=40)
-
 #other stuff
 window.title("Weblock")
 window.geometry("500x350+480+150")
 icon = tk.PhotoImage(file = 'src/icon.png')
 window.iconphoto(False, icon)
+
+#create labels
+title = tk.Label(window, text = "Website Blocker", font=("Helvetica", 18))
+title.place(x=150,y=40)
+
+info = tk.Label(window, text = "Make sure to run this program by right clicking \n and running as administrator. Enter website name below \n make sure to only put the webite name followed by the .com.\n For example \n youtube.com - Correct \n www.youtube.com - Wrong", font=("Sans Serif", 10))
+info.place(x=65,y=100)
+
+#change directory to "C:\Windows\System32\Drivers\etc"
+os.chdir(r"C:\Windows\System32\Drivers\etc")
+
+#main loop
 window.mainloop()
