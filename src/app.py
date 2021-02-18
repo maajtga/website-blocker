5cd5b4701c74a60f569b77a5ccc1a200d189eb52
maajtga <maajtga@users.noreply.github.com>
2021-02-18 10:29:28 -0500

    Update app.py

--- src/app.py
+++ src/app.py
@@ -14,21 +14,32 @@ window.iconphoto(False, icon)
 #change directory to "C:\Windows\System32\Drivers\etc"
 os.chdir(r"C:\Windows\System32\Drivers\etc")
 
+#create function to edit the hosts file
+def EditHost():
+    edit = open("hosts", "w")
+    t = edit.read()
+    print(t)
+
 #create labels
 title = tk.Label(window, text = "Website Blocker", font=("Helvetica", 18))
-title.place(x=150,y=40)
+title.place(x=150, y=40)
 
 directions = tk.Label(window, text = "Make sure to run this program by right clicking \n and running as administrator. Enter website name below \n make sure to only put the webite name followed by the .com.\n For example: \n youtube.com - Correct \n www.youtube.com - Wrong", font=("Sans Serif", 10))
-directions.place(x=65,y=100)
+directions.place(x=65, y=100)
 
 info = tk.Label(window, text = "Enter Website Here", font=("Sans Serif", 12))
-info.place(x=170,y=220)
+info.place(x=170, y=220)
+
 #website variable 
 website = tk.StringVar()
 
 #create input box
 web_entry = tk.Entry(window, textvariable = website, font = ('calibre', 10 ,'normal'))
-web_entry.place(x=170,y=250)
+web_entry.place(x=170, y=250)
+
+#create submit button
+sub_btn = tk.Button(window, text = 'Submit', command = EditHost)
+sub_btn.place(x=215, y=280)
 
 #main loop
 window.mainloop()
