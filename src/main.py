3f1bd52941f6a30c1e738afaa291f2f820f2943b
maajtga <maajtga@users.noreply.github.com>
2021-02-18 08:51:42 -0500

    Update main.py

---
+++ src/main.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+import os
+import sys
+
+#initialize the window
+window = tk.Tk()
+
+#other stuff
+window.title("Weblock")
+window.geometry("500x350+480+150")
+window.mainloop()
